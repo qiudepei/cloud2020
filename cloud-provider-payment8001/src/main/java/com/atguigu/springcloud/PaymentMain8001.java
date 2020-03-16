5dbe2781a01314d85d8b94a2f7fe46e7bb6caecc
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-16 19:04:43 +0800

    服务可以运行
    可以get到数据

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class PaymentMain8001 {
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentMain8001.class,args);
+	}
+}
