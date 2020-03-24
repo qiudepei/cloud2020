8490b07878d929d60ffa2f843600548239463874
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-24 19:51:26 +0800

    改动父工程druid版本
    改动父工程druid版本到1.1.16

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -21,7 +21,7 @@ public class PaymentController {
 	@PostMapping(value="/payment/create")
 	public CommonResult create(Payment payment){
 		int result = paymentService.create(payment);
-		log.info("===========插入结果："+result);
+		log.info("=======插入结果："+result);
 		if(result>0){
 			return new CommonResult(200,"插入数据成功",result);
 		}else {
