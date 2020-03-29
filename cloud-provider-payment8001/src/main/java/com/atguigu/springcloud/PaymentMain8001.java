c7c7090f96bedcb9c39c8e883caf21bda17aaa2c
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-29 16:24:21 +0800

    添加eureka单机版
    3个服务，1个euraka注册中心，1个provider,1个consumer

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class PaymentMain8001 {
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentMain8001.class,args);
