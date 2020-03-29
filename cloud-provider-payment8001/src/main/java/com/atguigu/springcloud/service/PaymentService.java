eee52602840309f75e7e3f3ea394882bdf2cf29e
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-29 15:24:23 +0800

    前四章基础完成
    重装了mysql，保证和阳哥版本一致。
    发现PAYMENT_URL和视频的不一样，解决了cloud-consumer-order80一直请求失败的问题

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -4,6 +4,7 @@ import com.atguigu.springcloud.entities.Payment;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Service;
 
+
 public interface PaymentService {
 	public int create(Payment payment);
 	public Payment getPaymentById(@Param("id") long id);
