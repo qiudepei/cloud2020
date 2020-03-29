df105b210bbc29c9bca18702c304e7f965ed2e85
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-30 00:00:01 +0800

    provider集群创建
    增加provider8002，出现bug，8001和8002的Pyment引用全部出错:com.atguigu.com.atguigu...

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -1,9 +1,7 @@
 package com.atguigu.springcloud;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
