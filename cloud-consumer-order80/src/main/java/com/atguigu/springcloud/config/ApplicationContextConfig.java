33ce2a7be98060b7a216b84b6483157e184d469a
qiudepei <35922616+qiudepei@users.noreply.github.com>
2020-03-30 18:48:54 +0800

    loadbanlance  负载均衡
    @Loadbanlance
    
    consumer不指定具体微服务，只指定集群服务名

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,5 +1,6 @@
 package com.atguigu.springcloud.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 public class ApplicationContextConfig
 {
     @Bean
+    @LoadBalanced   //负载均衡，因为不止一个provider，需要开启//
     public RestTemplate getRestTemplate()
     {
         return new RestTemplate();
